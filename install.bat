a2011dbfce9b76fcea65929175f247c7b952b1e9
Scott Carroll <30302797+carrols@users.noreply.github.com>
2019-03-15 17:50:07 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 echo "install.bat worked" >C:/worked.txt
 
 c:
-cd inetroot\wwwroot
+cd \inetpub\wwwroot
 
 powershell "$o=Get-Content appsettings.json | ConvertFrom-Json ; $o.DemoConfig.serverName = $env:computerName ; $o.DemoConfig.DbCon = \"Server=$Env:DB_HOSTNAME;Database=$Env:DB_DATABASE;User Id=$Env:DB_USERNAME;Password=$Env:DB_PASSWORD;\" ; $o | ConvertTo-Json | set-content appsettings.json"
